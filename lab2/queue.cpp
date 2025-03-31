f38a20025857d309a9f7384029b2c52120a983dd
rJLe6 <gg.gleb.gubarev@yandex.ru>
2025-03-31 18:55:37 +0000

    Laba 2 (tupoy github slozhna)

---
+++ lab2/queue.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+using namespace std::chrono;
+
+size_t strategy_add_one(size_t current_capacity) {
+    return current_capacity + 1;
+}
+
+size_t strategy_fixed_chunk(size_t current_capacity) {
+    const size_t chunk_size = 100;
+    return current_capacity + chunk_size;
+}
+
+size_t strategy_multiplicative(size_t current_capacity) {
+    if (current_capacity == 0) {
+        return 1;
+    }
+    return current_capacity * 2;
+}
+
+void test_strategy(const string& strategy_name, size_t (*strategy_func)(size_t), size_t max_elements) {
+    vector<int> array;
+    size_t allocated_size = 0;
+    size_t size = 0;
+    
+    
+    for (size_t i = 0; i < max_elements; ++i) {
+        if (size == allocated_size) {
+            auto start = high_resolution_clock::now();
+            
+            size_t new_capacity = strategy_func(allocated_size);
+            vector<int> new_array(new_capacity);
+            
+            for (size_t j = 0; j < size; ++j) {
+                new_array[j] = array[j];
+            }
+            
+            array = move(new_array);
+            allocated_size = new_capacity;
+            
+            auto end = high_resolution_clock::now();
+            auto duration = duration_cast<nanoseconds>(end - start);
+            
+            cout << i << "," << duration.count() << endl;
+        }
+        
+        if (size < array.size()) {
+            array[size] = i;
+        }
+        size+=1;
+    }
+}
+
+int main() {
+    const size_t max_elements = 100000;
+    //test_strategy("Add one", strategy_add_one, max_elements);
+    //test_strategy("Fixed chunk", strategy_fixed_chunk, max_elements);
+    test_strategy("Multiplicative", strategy_multiplicative, max_elements);
+}
